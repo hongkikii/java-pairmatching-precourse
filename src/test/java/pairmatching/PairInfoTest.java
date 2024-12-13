802775436d75a7a2f200b6d08fe2dd6de8e63bd1
mimi <110226866+hongkikii@users.noreply.github.com>
2024-12-14 00:36:04 +0900

    test(PairInfoTest): 페어 매칭 테스트 코드 작성

--- src/test/java/pairmatching/PairInfoTest.java
+++ src/test/java/pairmatching/PairInfoTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class PairInfoTest {
     @DisplayName("같은 레벨에서 이전에 매칭되었던 사람과 또 매칭될 시, 재매칭을 3회까지 시도한다.")
     @Test
-    void 재매칭_테스트() {
+    void 같은_레벨_재매칭_테스트() {
         // Given
         MockTwoPairInfo mockTwoPairInfo = new MockTwoPairInfo();
         MockThreePairInfo mockThreePairInfo = new MockThreePairInfo();
@@ -27,5 +27,23 @@ public class PairInfoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 매칭에 실패했습니다.");
         assertEquals(mockThreePairInfo.tryCount, 3);
+    }
+
+    @DisplayName("다른 레벨에서 이전에 매칭되었던 사람과 또 다시 매칭될 수 있다.")
+    @Test
+    void 다른_레벨_페어_매칭_테스트() {
+        // Given
+        MockTwoPairInfo mockTwoPairInfo = new MockTwoPairInfo();
+        MockThreePairInfo mockThreePairInfo = new MockThreePairInfo();
+
+        // When
+        Section section = new Section(Course.BACKEND, Level.LEVEL2, Mission.BASKET);
+
+        // Then
+        assertDoesNotThrow(() -> mockTwoPairInfo.match(section));
+        assertEquals(mockTwoPairInfo.tryCount, 1);
+
+        assertDoesNotThrow(() -> mockThreePairInfo.match(section));
+        assertEquals(mockThreePairInfo.tryCount, 1);
     }
 }
